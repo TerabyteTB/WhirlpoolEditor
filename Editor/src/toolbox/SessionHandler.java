7a414e96172344e0c90f179d54671e5898551498
TerabyteTB <raymondminecraft101@gmail.com>
2021-03-12 16:58:46 -0800

    set dir and enables it to read json

--- Editor/src/toolbox/SessionHandler.java
+++ Editor/src/toolbox/SessionHandler.java
@@ -1,28 +1,78 @@
 package toolbox;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Map;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public final class SessionHandler {
 	
+	private static String dir = "C:\\Apps\\SimpleJ2\\";
+	
 	private SessionHandler() {}
 	
-	public static void startSession() throws IOException {
+	public static Session newSession() {
 		
-		int id = 0;
-		ObjectMapper mapper = new ObjectMapper();
 		Session session = new Session();
-		String dir = "C:\\Apps\\JSimple\\";
 		File sessionDir = new File(dir);
 		sessionDir.mkdirs();
-		File sessionFile = new File(dir + "session.ssn");
-		if(sessionFile.exists()) {
-			JSONFormatter.formatAndPrintJSON("C:\\Apps\\SimpleJ2\\", "session.ssn", session);
-		} else {
+		File sessionFile = new File(dir + "session_" + session.getSessionID() +".ssn");
+		try {
 			sessionFile.createNewFile();
-			JSONFormatter.formatAndPrintJSON("C:\\Apps\\SimpleJ2\\", "session.ssn", session);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		JSONFormatter.formatAndPrintJSON(dir, "session_" + session.getSessionID() +".ssn", session);
+		
+		return session;
+		
+	}
+	
+public static Session newSession(String value) {
+		
+		Session session = new Session(value);
+		File sessionDir = new File(dir);
+		sessionDir.mkdirs();
+		File sessionFile = new File(dir + "session_" + session.getSessionID() +".ssn");
+		try {
+			sessionFile.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		JSONFormatter.formatAndPrintJSON(dir, "session_" + session.getSessionID() +".ssn", session);
+		
+		return session;
+		
+	}
+	
+	public static String openExistingSession(int id) {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String value = "none";
+		File sessionFile = new File("session_" + id + ".ssn");
+		
+		try {
+			Map<?, ?> map = mapper.readValue(new File(dir + sessionFile), Map.class);
+			for(Map.Entry<?, ?> entry : map.entrySet()) {
+				if(entry.getKey().equals("value")) {
+					value = (String) entry.getValue();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return value;
+		
+	}
+	
+	public static void removeSession(Session session) {
+		
+		File sessionFile = new File(dir + "session_" + session.getSessionID() + ".ssn");
+		Session.id = session.getSessionID();
+		sessionFile.delete();
 		
 	}
 
