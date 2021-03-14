197b2bb8b81818ad0a7043095e06a40d2e5fb9c9
TerabyteTB <raymondminecraft101@gmail.com>
2021-03-13 20:40:51 -0800

    update build script

--- BUILD.bat
+++ BUILD.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 echo 'building'
-if %1=='main'  (echo 'building main... Build successful') 
+if %1%=='main' (echo 'building main... Build successful') 
 else (echo 'Fatal: no subject selected')
 EXIT
