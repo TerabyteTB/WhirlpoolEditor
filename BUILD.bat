a65494562ac5dbda3e5fe40922ab688e056083ac
TerabyteTB <raymondminecraft101@gmail.com>
2021-03-13 20:39:01 -0800

    add build script for windows

--- BUILD.bat
+++ BUILD.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 echo 'building'
-if %1=='main' (echo 'building main... Build successful') else (echo 'Fatal: no subject selected')
+if %1=='main'  (echo 'building main... Build successful') 
+else (echo 'Fatal: no subject selected')
 EXIT
