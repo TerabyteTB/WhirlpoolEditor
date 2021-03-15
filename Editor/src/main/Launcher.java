9feb0b3ece624558de0b42603dbe651c4001523b
TerabyteTB <raymondminecraft101@gmail.com>
2021-03-15 14:15:30 -0700

    use lamda ex to make thread

--- Editor/src/main/Launcher.java
+++ Editor/src/main/Launcher.java
@@ -1,11 +1,23 @@
 package main;
 
+import java.io.IOException;
+
+import editor.TextEdit;
+
 public class Launcher {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		
-		Main main = new Main();
+		Thread thread = new Thread(() -> {
+			try {
+				TextEdit editor = new TextEdit();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+		
+		thread.start();
 
 	}
 
