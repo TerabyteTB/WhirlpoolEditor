20fd6d65ecd7d6462da2ea3c6aa2283abb9a7146
TerabyteTB <raymondminecraft101@gmail.com>
2021-03-16 21:14:57 -0700

    remove unused import (i hate online useless suggestions)

--- Editor/src/editor/TextEdit.java
+++ Editor/src/editor/TextEdit.java
@@ -1,7 +1,6 @@
 package editor;
 
 import java.awt.BorderLayout;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
