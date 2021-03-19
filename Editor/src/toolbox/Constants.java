02264784ecc570e203835269f75eae266ca04771
TerabyteTB <raymondminecraft101@gmail.com>
2021-03-18 19:15:53 -0700

    add constants class

---
+++ Editor/src/toolbox/Constants.java
@@ -0,0 +1,20 @@
+package toolbox;
+
+import java.util.logging.Logger;
+/**
+ * 
+ * Constants class
+ * @author TerabyteTB
+ * @version 1.0
+ *
+ */
+public final class Constants {
+	
+	private Constants() {}
+	
+	/**
+	 * Logger for all classes (in case i ever need it)
+	 */
+	public static final Logger LOGGER = Logger.getGlobal();
+
+}
