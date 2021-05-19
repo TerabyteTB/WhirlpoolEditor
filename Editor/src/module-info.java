d54b35a10c7a6bbb5c28a554f56eface222afa63
TerabyteTB <raymondan0831@gmail.com>
2021-05-18 19:34:22 -0700

    Modularization!!!

---
+++ Editor/src/module-info.java
@@ -0,0 +1,10 @@
+module io.terabyetb.editor {
+	exports editor;
+	exports utils;
+	exports utils.session;
+	exports main;
+
+	requires com.fasterxml.jackson.core;
+	requires com.fasterxml.jackson.databind;
+	requires java.se;
+}
